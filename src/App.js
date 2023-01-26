56728bdd538458482e65fb8c332b4fff33e0d138
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-01-26 22:02:07 +0900

    feat: implement routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Logo } from './components/Logo';
-import { Phrase } from './components/Phrase';
-import { SignApple } from './components/SignApple';
+import { Routes, Route } from 'react-router-dom';
+import Init from './page/Init';
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <Phrase />
-      <SignApple />
+      <Routes>
+        <Route exact path='/' element={<Init />} />
+        {/* <Route path='/create-routine' element={<CreateRoutine />} />
+        <Route path='/:routine-set' element={<ToDo />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/page/Init.js
@@ -0,0 +1,13 @@
+import { Logo } from '../components/Logo';
+import { Phrase } from '../components/Phrase';
+import { SignApple } from '../components/SignApple';
+
+export default function Init() {
+    return (
+        <>
+            <Logo />
+            <Phrase />
+            <SignApple />
+        </>
+    );
+}
